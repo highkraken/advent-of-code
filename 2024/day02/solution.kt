fa92eb624bcdbb353585e371bf255982e6fd4ea5
highkraken <90514516+highkraken@users.noreply.github.com>
2024-12-03 00:13:02 +0530

    solve day 2

---
+++ 2024/day02/solution.kt
@@ -0,0 +1,28 @@
+package day
+
+import java.io.File
+import aocutils.*
+
+private const val INPUT = "day/input.txt"
+private const val DEMO_INPUT = "day/demo_input.txt"
+private var actual = false
+private const val PART1 = "Part 1: "
+private const val PART2 = "Part 2: "
+
+private fun readAndParseInput() {
+    val file = File(if (actual) INPUT else DEMO_INPUT)
+
+    file.forEachLine { line ->
+
+    }
+}
+
+fun solve() {
+
+}
+
+private fun main()  {
+    actual = false
+    val input = readAndParseInput()
+
+}
